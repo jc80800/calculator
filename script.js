b4bb502e31df2da912ee5a85a1c2113ebaf8d2a9
jc8080 <jc8080@rit.edu>
2020-06-25 15:08:59 -0400

    Made a box in which results will be shown in

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 const container = document.getElementById("container");
-const bar = document.querySelector('.bar');
+const display = document.querySelector('.display');
 const operatorButtons = document.querySelector('.operatorButtons');
 
-function makeBar(){
-    bar.style.setProperty
+function makeDisplay(){
     let bar = document.createElement("div");
     bar.innerHTML = "10 + 9 = 21";
-    calculationBar.appendChild(bar).className = "bar-item";
+    display.appendChild(bar).className = "display-item";
 }
 
 
+
+
+
+function operate(operator, num1, num2){
+    if (operator === "add" ){
+        add(num1, num2);
+    }
+    else if (operator === "subtract"){
+        subtract(num1, num2);
+    }
+    else if (operator === "multiply"){
+        multiply(num1, num2);
+    } 
+    else if (operator === "divide"){
+        divide(num1, num2);
+    }
+    else{
+        console.log("error");
+    }
+}
 
 function add(num1, num2){
     return num1 + num2;
@@ -27,4 +46,4 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
-makeBar();
+makeDisplay();
