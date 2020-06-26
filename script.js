456dc29afadfad2a6349082a4549794d45eb0c6f
jc8080 <jc8080@rit.edu>
2020-06-26 11:15:49 -0400

    Created operate button that takes in a string, prompts for 2 numbers, returns result and manipulate display

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
 const container = document.getElementById("container");
 const display = document.querySelector('.display');
-const operatorButtons = document.querySelector('.operatorButtons');
+const buttons = document.querySelector('.operatorBtns');
 
 function makeDisplay(){
     let bar = document.createElement("div");
-    bar.innerHTML = "10 + 9 = 21";
+    bar.innerText = "Answer will be displayed here"
     display.appendChild(bar).className = "display-item";
 }
 
+function changeDisplay(result){
+    let bar = document.createElement("div");
+    bar.innerText = result;
+    const oldChild = document.querySelector('.display-item');
+    display.replaceChild(bar, oldChild);
+    display.appendChild(bar).className = "display-item";
+}
+
+function addBtns(){
+    let addBtns = document.createElement("button");
+    addBtns.innerText = "Operate";
+    addBtns.addEventListener('click', () => {
+        let num1 = window.prompt("Choose number 1:");
+        let num2 = window.prompt("Choose number 2:");
+        let operator = window.prompt("Which operation:");
+        let result = operate(operator, num1, num2);
+        changeDisplay(result);
+    });
+
+    buttons.appendChild(addBtns).className = "button";
+    
+}
 
 
 
 
 function operate(operator, num1, num2){
     if (operator === "add" ){
-        add(num1, num2);
+        return add(num1, num2);
     }
     else if (operator === "subtract"){
-        subtract(num1, num2);
+        return subtract(num1, num2);
     }
     else if (operator === "multiply"){
-        multiply(num1, num2);
+        return multiply(num1, num2);
     } 
     else if (operator === "divide"){
-        divide(num1, num2);
+        return divide(num1, num2);
     }
     else{
         console.log("error");
@@ -31,19 +53,20 @@ function operate(operator, num1, num2){
 }
 
 function add(num1, num2){
-    return num1 + num2;
+    return parseInt(num1) + parseInt(num2);
 }
 
 function subtract (num1, num2){
-    return num1 - num2;
+    return parseInt(num1) - parseInt(num2);
 }
 
 function multiply(num1, num2){
-    return num1 * num2;
+    return parseInt(num1) * parseInt(num2);
 }
 
 function divide(num1, num2){
-    return num1 / num2;
+    return parseInt(num1) / parseInt(num2);
 }
 
 makeDisplay();
+addBtns();
