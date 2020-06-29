c187de812cade6b5dc518360545b3d18373c9693
jc8080 <jc8080@rit.edu>
2020-06-29 11:18:17 -0400

    Added a button that represents 1. Created a string that can be manipulated along with a function that takes in the string and splits. The function console.log the result of the operation

--- script.js
+++ script.js
@@ -16,19 +16,37 @@ function changeDisplay(result){
     display.appendChild(bar).className = "display-item";
 }
 
+let expression = "";
+
 function addBtns(){
     let addBtns = document.createElement("button");
-    addBtns.innerText = "Operate";
+    addBtns.innerText = "1";
     addBtns.addEventListener('click', () => {
-        let num1 = window.prompt("Choose number 1:");
-        let num2 = window.prompt("Choose number 2:");
-        let operator = window.prompt("Which operation:");
-        let result = operate(operator, num1, num2);
-        changeDisplay(result);
+        expression = expression + 1;
+        console.log(expression);
     });
-
     buttons.appendChild(addBtns).className = "button";
-    
+}
+
+function addOperate(num1){
+    let additionBtn = document.createElement("button");
+    additionBtn.innerText = "+";
+    additionBtn.addEventListener('click', () => {
+        expression = expression + " + ";
+        console.log(expression);
+    })
+    buttons.appendChild(additionBtn).className = "button";
+}
+
+function readString(){
+    let operate = document.createElement("button");
+    operate.addEventListener('click', () => {
+        stringArray = expression.split(" ");
+        let num1 = parseInt(stringArray[0]);
+        let num2 = parseInt(stringArray[2]);
+        console.log(num1 + num2);
+    })
+    buttons.appendChild(operate).className = "button";
 }
 
 
@@ -70,3 +88,5 @@ function divide(num1, num2){
 
 makeDisplay();
 addBtns();
+addOperate();
+readString();
